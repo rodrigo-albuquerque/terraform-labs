2cf897863396eaddd43e60838fbf6eec1f88a336
rodrigo-albuquerque <digofarias@gmail.com>
2020-02-15 14:23:21 +0000

    corrected errors in main.cf

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
-resource "aws_vpc" "dc_tmp_vpc" {
+resource "aws_vpc" "albuquerque_vpc" {
   cidr_block = "172.16.199.0/24"
 
   tags = {
-    Name = "albuquerque-VPC"
+    Name = "albuquerque_vpc"
   }
 }
 
-resource "aws_subnet" "dc_tmp_subnet" {
-  vpc_id = aws_vpc.dc_tmp_vpc.id
+resource "aws_subnet" "albuquerque_subnet" {
+  vpc_id = aws_vpc.albuquerque_vpc.id
   cidr_block = "172.16.199.0/25"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "albuquerque-dc_tmp_subnet"
+    Name = "albuquerque_subnet"
   }
 }
 
-resource "aws_security_group" "dc_tmp-allow_tls_http_and_ssh" {
+resource "aws_security_group" "albuquerque_sg" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.dc_tmp_vpc.id
+  vpc_id      = aws_vpc.albuquerque_vpc.id
 
   ingress {
     # SSH,
@@ -53,7 +53,7 @@ resource "aws_security_group" "dc_tmp-allow_tls_http_and_ssh" {
   }
 
   tags = {
-    Name = "dc_tmp_allow_all"
+    Name = "albuquerque_sg"
   }
 }
 
@@ -61,10 +61,10 @@ resource "aws_instance" "albuquerque-debian-nginx-1" {
   ami                         = data.aws_ami.debian.id
   instance_type               = "t2.micro"
   key_name                    = "dckeypair"
-  subnet_id                   = aws_subnet.dc_tmp_subnet.id
-  vpc_security_group_ids      = [aws_security_group.dc_tmp-allow_tls_http_and_ssh.id]
+  subnet_id                   = aws_subnet.albuquerque_subnet.id
+  vpc_security_group_ids      = [aws_security_group.albuquerque_sg.id]
   associate_public_ip_address = true
   tags = {
-    Name = "Server1"
+    Name = "albuquerque-nginx1"
   }
 }
